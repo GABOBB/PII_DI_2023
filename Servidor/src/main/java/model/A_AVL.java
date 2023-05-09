67982b15e9b23514fd7aa902c5f73971a0401136
Yerik Chaves <yerikchaves818@gmail.com>
2023-05-09 11:02:42 -0600

    Se comienza el desarrollo del arbol AVL v1.3.2

--- Servidor/src/main/java/model/A_AVL.java
+++ Servidor/src/main/java/model/A_AVL.java
@@ -8,6 +8,38 @@ package model;
  *
  * @author Gabriel
  */
-public class A_AVL {
+public class A_AVL extends A_B_B{
     
+    
+    private N_AVL rotateLeft(N_AVL node) {
+        N_AVL x = node.getRt();
+        N_AVL T2 = x.getLt();
+
+        x.setLt(node);
+        node.setRt(T2);
+
+        node.height = 1 + Math.max(height(node.left), height(node.right));
+        x.height = 1 + Math.max(height(x.left), height(x.right));
+
+        node.size = 1 + size(node.left) + size(node.right);
+        x.size = 1 + size(x.left) + size(x.right);
+
+        return x;
+    }
+
+    private Node rotateRight(Node node) {
+        Node x = node.left;
+        Node T2 = x.right;
+
+        x.right = node;
+        node.left = T2;
+
+        node.height = 1 + Math.max(height(node.left), height(node.right));
+        x.height = 1 + Math.max(height(x.left), height(x.right));
+
+        node.size = 1 + size(node.left) + size(node.right);
+        x.size = 1 + size(x.left) + size(x.right);
+
+        return x;
+    }
 }
